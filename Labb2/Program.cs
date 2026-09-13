77cf7cb08d8f309c825747ef1b7a07a68bcb2f4a
Tronje1 <tronje.skoglund1@gmail.com>
2026-09-13 21:31:03 +0200

    Add project files.

---
+++ Labb2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Labb2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //◼︎◻︎
+            Console.Write("Number: ");
+            int num = Int32.Parse(Console.ReadLine());
+            int max = num * num;
+            int rutor = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    if (rutor % 2 == 1)
+                    {
+                        Console.Write("o ");
+                    }
+                    else
+                    {
+                        Console.Write("x ");
+                    }
+                    rutor++;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
